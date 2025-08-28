da153cb56c5bc1f8f3dd06970b2f606ca4910e9d
TheRealWushuMaster <chiossi@gmail.com>
2025-08-28 11:35:48 -0300

    Tweaked minimal simulation visualizations.

--- examples/minimal_simulation.py
+++ examples/minimal_simulation.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from simulation.simulator import Simulator
 from examples.battery_and_motor_only import minimalistic_em_vehicle
 
-time_steps: int = 800
+time_steps: int = 2_000
 control_signal: list[float] = [1.0] * time_steps
 
 simulation = Simulator(time_steps=time_steps,
@@ -39,13 +39,22 @@ ax3 = axes[2]
 ax4 = axes[3]
 
 ax1.plot(motor_rpms)
-ax1.set_title("RPM")
+ax1.set_ylim(bottom=0.0)
+ax1.set_title("RPM Out")
+ax1.set_ylabel("RPM")
 ax2.plot(motor_power_in)
+ax2.set_ylim(bottom=0.0)
 ax2.set_title("Power In")
+ax2.set_ylabel("Power (W)")
 ax3.plot(motor_power_out)
+ax3.set_ylim(bottom=0.0)
 ax3.set_title("Power Out")
+ax3.set_ylabel("Power (W)")
 ax4.plot(motor_torque_out)
+ax4.set_xlim(left=0.0, right=time_steps)
+ax4.set_ylim(bottom=0.0)
 ax4.set_title("Torque Out")
+ax4.set_xlabel("Time steps")
 for ax in axes:
     ax.grid()
 fig.tight_layout()
