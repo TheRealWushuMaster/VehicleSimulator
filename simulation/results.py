addb470f65e83abc0de4644189c292ad607d7be9
TheRealWushuMaster <chiossi@gmail.com>
2025-09-20 03:49:54 -0300

    Improved plot formatting

--- simulation/results.py
+++ simulation/results.py
@@ -6,14 +6,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from simulation.simulator import Simulator
 
-NICE_LABELS: dict[str, tuple[str, str]] = {"rpm_in": ("Speed (RPM)", "Input axis rotation"),
-                                           "rpm_out": ("Speed (RPM)", "Output axis rotation"),
+NICE_LABELS: dict[str, tuple[str, str]] = {"rpm_in": ("Speed (RPM)", "Input speed"),
+                                           "rpm_out": ("Speed (RPM)", "Output speed"),
                                            "torque_in": ("Torque (N.m)", "Input torque"),
                                            "torque_out": ("Torque (N.m)", "Output torque"),
                                            "power_in": ("Power (W)", "Input power"),
                                            "power_out": ("Power (W)", "Output power"),
                                            "temperature": ("Temperature (K)", "Temperature"),
-                                           "on": ("On/Off", "On/Off status"),
+                                           "on": ("Status (On/Off)", "Status"),
                                            "electric_energy_stored": ("Energy (J)", "Electric energy stored"),
                                            "fuel_liters_in": ("Fuel flow (liters)", "Input fuel flow"),
                                            "fuel_liters_out": ("Fuel flow (liters)", "Output fuel flow"),
@@ -59,33 +59,57 @@ class ResultsManager():
         """
         return self.dataframes.get(comp_type, pd.DataFrame())
 
-    def plot_all(self):
+    def plot_all(self, num_cols: int=1,
+                 folder: str="examples/results"):
         """
         Plots all DataFrames with subplots per variable.
         """
+        folder = f"{folder}/{self.simulation.name}"
+        os.makedirs(folder, exist_ok=True)
+        num_cols = max(num_cols, 1)
         for comp_type, df in self.dataframes.items():
-            if not df.empty:
-                df = df.set_index("sim_time").drop(columns=["comp_id", "time_step"])
-                axes = df.plot(subplots=True,
-                               layout=(-1, 2),
-                               figsize=(10, 6),
-                               legend=True,
-                               sharex=True)
+            if df.empty:
+                continue
+            t_min = df.sim_time.min()
+            t_max = df.sim_time.max()
+            for name in df.comp_name.unique().tolist():
+                df_temp = df[df.comp_name==name].set_index("sim_time").drop(
+                    columns=["comp_id",
+                             "time_step",
+                             "comp_name"])
+                assert df_temp.empty is False
+                for col in df_temp.columns:
+                    if df_temp[col].dtype==bool:
+                        df_temp[col] = df_temp[col].astype(int)
+                axes = df_temp.plot(subplots=True,
+                                    layout=(-1, num_cols),
+                                    figsize=(6, 10),
+                                    legend=False,
+                                    sharex=True,
+                                    xlim=(t_min, t_max),
+                                    title=f"Simulation data for '{name}'\nType = '{comp_type}'")
                 axes = axes.flatten()
-            for ax, col in zip(axes, df.columns):  # type: ignore
-                y_label = NICE_LABELS.get(col, col.replace("_", " "))[0].title()
-                title = NICE_LABELS.get(col, col.replace("_", " "))[1].title()
-                ax.set_title(f"{comp_type} - {title}")
-                ax.set_xlabel("Time (s)")
-                ax.set_ylabel(y_label)
-                ax.grid(True)
-            plt.tight_layout()
+                for ax, col in zip(axes, df_temp.columns):
+                    y_label = NICE_LABELS.get(col, col.replace("_", " "))[0]  # type: ignore
+                    title = NICE_LABELS.get(col, col.replace("_", " "))[1]  # type: ignore
+                    ax.set_title(title)
+                    ax.set_xlabel("Time (s)")
+                    ax.set_ylabel(y_label)
+                    ax.grid(True)
+                plt.tight_layout()
+                plt.savefig(f"{folder}/{self.simulation.name}-{comp_type}-{name.replace(" ", "_")}.png",
+                            dpi=300)
         plt.show()
 
-    def save_csv(self, folder="examples/results"):
+    def save_csv(self, folder: str="examples/results"):
         """
         Save each component type DataFrame to a CSV file.
         """
+        folder = f"{folder}/{self.simulation.name}"
         os.makedirs(folder, exist_ok=True)
         for comp_type, df in self.dataframes.items():
-            df.to_csv(f"{folder}/{self.simulation.name}_{comp_type}.csv", index=False)
+            if not df.empty:
+                column_names = {col: NICE_LABELS[col][1] if col in NICE_LABELS else col
+                                for col in df.columns}
+                df = df.rename(columns=column_names)
+                df.to_csv(f"{folder}/{self.simulation.name}-{comp_type}-data.csv", index=False)
